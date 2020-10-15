0290a62fb4dd3858cc050e39ace494bff8975b8c
liz0611 <53869386+liz0611@users.noreply.github.com>
2020-10-14 22:55:04 -0500

    practica herencia
    Agregué algunos comentarios y las partes pequeñas que faltaban :D

--- 12102020/estudiante.py
+++ 12102020/estudiante.py
@@ -1,50 +1,73 @@
 # clases persona, alumno profesor
 
-class Persona:
-    nombre = None
+class Persona: #creamos la clase persona 
+    nombre = None #los datos se muestran vacios
     ap = None
     am = None
     rfc = None
 
+  #declaramos el constructor
     def __init__(self, nombre=None, ap=None, am=None, rfc=None):
         self.nombre = nombre
         self.ap = ap
         self.am = am
         self.rfc = rfc
+  #agregamos una funcion para mostrar los datos obtenidos
+    def muestra(self):
+        print(40*"-")
+        print("nombre: ", self.nombre)
+        print("ap: ", self.ap)
+        print("am: ", self.am)
+        print("rfc: ", self.rfc)
 
-    def __muestra(self):
-        print("nombre: ", nombre)
-        print("ap: ", ap)
-        print("am: ", am)
-        print("rfc: ", rfc)
 
+#creamos la clase alumno, la clase Persona dentro indica la herencia
+class Alumno(Persona):
 
-class alumno(Persona):
-
-    def __init__(self, nombre=None, ap=None, am=None, rfc=None, matricula=None, semestre=None):
+    #declaramos el constructor
+    def __init__(self, nombre=None, ap=None, am=None, rfc=None, 
+                 matricula=None, semestre=None):
+        #llamanos los datos de la clase Persona
         super().__init__(nombre, ap, am, rfc)
+        
+        #datos pertenecientes solo al alumno
         self.matricula = matricula
         self.semestre = semestre
 
 
-class profesor(Persona):
-    def __init__(self, nombre=None, ap=None, am=None, rfc=None, matricula=None, area=None, horasClase=None):
-        super(profesor, self).Persona
+#Declaramos la clase profesor que hereda de Persona
+class Profesor(Persona):
+    def __init__(self, nombre=None, ap=None, am=None, rfc=None, 
+                 matricula=None, area=None, horasClase=None):
+        
+        #llamamos datos de la clase Persona
+        super().__init__(nombre, ap, am, rfc)
+
+        #datos pertenecientes solo al profesor
         self.matricula = matricula
         self.area = area
         self.horasClase = horasClase
 
- # def muestra(self):
 
 # principal
 
+#creamos un objeto de cada clase
+toño = Alumno("Antonio", "Perez", "Lopez", None, "123456789", 6)
 
-toño = alumno("toño", "perez", "lopez", None, "123456789", 6)
+Javi = Profesor("Javier", "Castillo", "Perez",3261, 4815162342,
+                "Matemáticas", 36)
 
-#toño = profesor("Javier", "Castillo", "Perez",
- #               3261, 4815162342, "Matemáticas", 36)
-
+#probamos la herencia en ambas clases llamando a la funcion de
+#la clase Persona muestra()
 toño.muestra()
+Javi.muestra()
+
+
+
+
+
+
+
 
 # checar el apartado SUPER EN PYTHON
 
